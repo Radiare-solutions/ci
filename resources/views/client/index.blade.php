ccfc16be6b1f9a87ba42c9e4f104bf2b45f4bfc3
Jagadeeswaran A <jagadeeswaran.a@radiare.com>
2016-04-24 13:50:02 +0530

    common menubar

--- resources/views/client/index.blade.php
+++ resources/views/client/index.blade.php
@@ -14,31 +14,7 @@ $level1 = json_decode($level1Details);
     <div class="sidebar sidebar-main sidebar-default sidebar-fixed bg-blue-800">
         <div class="sidebar-content">
             <!-- Main navigation -->
-            <div class="sidebar-category sidebar-category-visible">
-                <div class="category-content no-padding">
-                    <ul class="navigation navigation-main navigation-accordion">
-                        <li><a href="home.html"><i class="icon-home4"></i> <span>Home</span></a></li>
-                        <li>
-                            <a href="#"><i class="icon-stack2"></i> <span>Category Management</span></a>
-                            <ul>
-                                <li><a href="molecules.html">Molecule</a></li>
-                                <li><a href="indication.html">Indication</a></li>
-                                <li class="active"><a href="client.html">Client</a></li>
-                            </ul>
-                        </li>
-                        <li><a href="db-setup.html"><i class="icon-steam"></i> <span>Client Setup</span></a></li>
-                        <li>
-                            <a href="#"><i class="icon-database-arrow"></i> <span>Verify</span></a>
-                            <ul>
-                                <li><a href="product.html">Product</a></li>
-                                <li><a href="condition.html">Condition</a></li>
-                                <li><a href="sponsor.html">Sponsor</a></li>
-                            </ul>
-                        </li>
-
-                    </ul>
-                </div>
-            </div>
+            @include('layouts/sidebar')
             <!-- /main navigation -->
 
         </div>
---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,25 @@
+<div class="sidebar-category sidebar-category-visible">
+    <div class="category-content no-padding">
+        <ul class="navigation navigation-main navigation-accordion">
+            <li><a href="home.html"><i class="icon-home4"></i> <span>Home</span></a></li>
+            <li>
+                <a href="#"><i class="icon-stack2"></i> <span>Category Management</span></a>
+                <ul>
+                    <li><a href="molecules.html">Molecule</a></li>
+                    <li><a href="indication.html">Indication</a></li>
+                    <li><a href="client.html">Client</a></li>
+                </ul>
+            </li>
+            <li><a href="db-setup.html"><i class="icon-steam"></i> <span>Client Setup</span></a></li>
+            <li>
+                <a href="#"><i class="icon-database-arrow"></i> <span>Verify</span></a>
+                <ul>
+                    <li><a href="product.html">Product</a></li>
+                    <li><a href="condition.html">Condition</a></li>
+                    <li><a href="sponsor.html">Sponsor</a></li>
+                </ul>
+            </li>
+
+        </ul>
+    </div>
+</div>
